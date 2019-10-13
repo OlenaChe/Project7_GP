6db1d6fb388e3df1e53c4e3f5088204cdd343b3d
Olena Androsovych <olenaandrosovych@macbook-air-olena.home>
2019-10-13 20:22:02 +0200

    changed views

--- GrandPy/views.py
+++ GrandPy/views.py
@@ -1,21 +1,21 @@
 """Views of the application GrandPy"""
 
 from flask import Flask, render_template, jsonify, request
-
+"""
 from .parseur import parse
 from .wiki_info import get_wiki_extract, get_wiki_url
 from .location import get_address, get_latitude, get_longitude
-
+"""
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     """Method which renders an html page"""
     return render_template("main.html")
-
+"""
 @app.route('/process/', methods=['POST'])
 def process():
-    """Method which renders the data for the processing with ajax"""
+    Method which renders the data for the processing with ajax
     question = request.form['question']
     parsed_question = parse(question)
     wiki_extract = get_wiki_extract(parsed_question)
@@ -26,4 +26,4 @@ def process():
     return jsonify({'data': [question, address, wiki_extract, wiki_url, lat, lng, parsed_question]})
 
 #if __name__ == "__main__":
-    #app.run(debug=True)
+    #app.run(debug=True)"""
